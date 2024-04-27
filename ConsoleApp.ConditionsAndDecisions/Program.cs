dd1407e8e7e12b64883ed9603463447e2c3e2685
bbbrock <btucker@btdevel.com>
2024-04-27 00:30:55 -0500

    Explored conditional and decision statements.

---
+++ ConsoleApp.ConditionsAndDecisions/Program.cs
@@ -0,0 +1,127 @@
+﻿namespace ConsoleApp.ConditionsAndDecisions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt for input
+            Console.Write("Please enter student's grade: ");
+            int grade = Convert.ToInt32(Console.ReadLine());
+
+            // Decide to print pass or fail based on input
+            Console.WriteLine("************** Simple if Results **************");
+            if(grade > 50)
+            {
+                Console.WriteLine("Student has passed.");
+            } else
+            {
+                Console.WriteLine("Student has failed.");
+                Console.WriteLine("Please recommend student to student affairs office.");
+            }
+            Console.WriteLine("************** End Simple if Results **************");
+
+            Console.WriteLine("************** Complex if..else if Results **************");
+            if (grade < 0 || grade > 100)
+            {
+                Console.WriteLine("Invalid value entered.");
+            }
+            else if (grade >= 0 && grade < 50)
+            {
+                Console.WriteLine("Student has failed - F");
+            }
+            else if (grade >= 50 && grade <= 64)
+            {
+                Console.WriteLine("C-");
+            }
+            else if (grade >= 65 && grade <= 74)
+            {
+                Console.WriteLine("C");
+            }
+            else if (grade >= 75 && grade <= 84)
+            {
+                Console.WriteLine("B");
+            }
+            else if (grade >= 85 && grade <= 100)
+            {
+                Console.WriteLine("A - Good job");
+            }
+            Console.WriteLine("************** End Complex if..else if Results **************");
+
+            Console.WriteLine("************** Ternary Results **************");
+
+            string passStatus = grade < 50 ? "Fail" : "Pass";
+            Console.WriteLine($"Your student status is: {passStatus}");
+
+            Console.WriteLine("************** End Ternary Results **************");
+
+            Console.Write("Please enter a day of the week [1-7] with Sunday as 1: ");
+            int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+            // Writing out what we're going to do with switch statement using if statements.
+            if (dayOfWeek <= 0 ||  dayOfWeek > 7)
+            {
+                Console.WriteLine("Invalid value entered.");
+            }
+            else if (dayOfWeek == 1)
+            {
+                Console.WriteLine("Sunday");
+            } else if (dayOfWeek == 2)
+            {
+                Console.WriteLine("Monday");
+            } else if (dayOfWeek == 3)
+            {
+                Console.WriteLine("Tuesday");
+            }
+            else if (dayOfWeek == 4)
+            {
+                Console.WriteLine("Wednesday (Hump Day!)");
+            }
+            else if (dayOfWeek == 5)
+            {
+                Console.WriteLine("Thursday");
+            }
+            else if (dayOfWeek == 6)
+            {
+                Console.WriteLine("Friday (TGIF!!!)");
+            }
+            else if (dayOfWeek == 7)
+            {
+                Console.WriteLine("Saturday (Beach Day!!!)");
+            }
+
+            Console.WriteLine("************** switch Results **************");
+            switch (dayOfWeek)
+            {
+                case 1: 
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2: 
+                    Console.WriteLine("Monday");
+                    break;
+                case 3: 
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4: 
+                    Console.WriteLine("Wednesday (Hump Day!)");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday (TGIF!!!");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday (Beach Day!!!)");
+                    break;
+                default:
+                    Console.WriteLine("Some other invalid value entered.");
+                    break;
+            }
+
+            Console.WriteLine("************** End switch Results **************");
+
+            // Rest of the application            
+            Console.WriteLine("Thank you for using this program.");
+        }
+    }
+}
---
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Welcome message
+Console.WriteLine("Welcome to the sample calculator!");
+
+// Prompt for user input
+Console.Write("Please enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Show calculator options
+Console.WriteLine("Please select the operation you would like to perform.");
+Console.WriteLine($"1: Addition - {num1} + {num2}");
+Console.WriteLine($"2: Subtraction - {num1} - {num2}");
+Console.WriteLine($"3: Multiplication - {num1} * {num2}");
+Console.WriteLine($"4: Division - {num1} / {num2}");
+Console.WriteLine($"5: Modulus (Remainder) - {num1} % {num2}");
+Console.WriteLine("");
+Console.Write("Please enter your selection [1-5]: ");
+int selection = Convert.ToInt32(Console.ReadLine());
+
+// Decide which operation is needed based on selected option
+int answer = 0;
+string operation = string.Empty;
+switch (selection)
+{
+    case 1: 
+        answer = num1 + num2; 
+        operation = "+"; 
+        break;
+    case 2: 
+        answer = num1 - num2;
+        operation = "-";
+        break;
+    case 3: 
+        answer = num1 * num2;
+        operation = "*";
+        break;
+    case 4: 
+        answer = num1 / num2;
+        operation = "/";
+        break;
+    case 5: 
+        answer = num1 % num2;
+        operation = "%";
+        break;
+    default: 
+        Console.WriteLine("You have entered an invalid selection."); 
+        break;
+}
+// Print output
+Console.WriteLine($"{num1} {operation} {num2} = {answer}");
